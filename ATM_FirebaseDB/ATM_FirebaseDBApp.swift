df0f3aa94c432ce248d771c0881b7fe6955eb1f0
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-07-20 17:03:10 +0200

    Initial Commit

---
+++ ATM_FirebaseDB/ATM_FirebaseDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATM_FirebaseDBApp.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/07/20.
+//
+
+import SwiftUI
+
+@main
+struct ATM_FirebaseDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
