ce244ba8cbf1ab6e7ed3c855edffb5cdb8c66b9f
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-07-21 08:35:56 +0200

    [ATM2.0Firebase][WIP] Made a destination file to link the login page to
    LoginView.swift needed a destination view for the sign-in button, code will be added once the database model is set up

---
+++ ATM_FirebaseDB/Views/RegisterNewUserView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterNewUserView.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/07/21.
+//
+
+import SwiftUI
+
+struct RegisterNewUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterNewUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterNewUserView()
+    }
+}
