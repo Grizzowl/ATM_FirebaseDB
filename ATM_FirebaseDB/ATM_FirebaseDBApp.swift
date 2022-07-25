3419d69e757c16176cb08aea3f4ced6e1c4f574b
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-07-25 11:41:39 +0200

    [ATM2.0Firebase][Feature] View handler setup
    This method of displaying the views allows the user to sign out of their account without stacking navigation bars on the top of the login screen, also when the app is closed it auto refreshes the app and will send the user back to the login screen for added security

--- ATM_FirebaseDB/ATM_FirebaseDBApp.swift
+++ ATM_FirebaseDB/ATM_FirebaseDBApp.swift
@@ -12,13 +12,35 @@ import FirebaseCore
 struct ATM_FirebaseDBApp: App {
     
     //Firebase setup
-    init() {
-        FirebaseApp.configure()
-    }
+//    init() {
+//        FirebaseApp.configure()
+//    }
+    
+    @StateObject var userStateViewModel = UserStateViewModel()
     
     var body: some Scene {
         WindowGroup {
+            NavigationView{
+                ApplicationSwitcher()
+            }
+            .navigationViewStyle(.stack)
+            .environmentObject(userStateViewModel)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct ApplicationSwitcher: View {
+    
+    @EnvironmentObject var vm: UserStateViewModel
+    
+    var body: some View {
+        if (vm.isLoggedIn) {
+            //HomeScreenView() [WIP] Set up a home screen for the user linked to firebase
+        } else {
             LoginView()
         }
+        
     }
 }
