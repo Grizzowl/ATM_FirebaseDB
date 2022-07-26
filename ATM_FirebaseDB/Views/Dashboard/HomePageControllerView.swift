6ad2ed469cc656e459dc1a57f6c6c206a1fbd37c
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-07-26 16:50:20 +0200

    [ATM2.0Firebase][WIP] Home page Work-in-Progress
    This view will handle and display various functions of the banking app

---
+++ ATM_FirebaseDB/Views/Dashboard/HomePageControllerView.swift
@@ -0,0 +1,24 @@
+//
+//  HomePageControllerView.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/07/26.
+//
+
+import SwiftUI
+
+/*
+ app home page HomePageControllerView
+ */
+
+struct HomePageControllerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePageControllerView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageControllerView()
+    }
+}
