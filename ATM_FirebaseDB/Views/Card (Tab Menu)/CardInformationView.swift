5c497f3b9a0a06685febecd35254fc1cf1040218
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-07-29 11:13:04 +0200

    [ATM2.0Firebase][Feature] User Card Information
    This View shows the user their card number, exp date, and CVV code along with an option to report a stolen card or request a new card

---
+++ ATM_FirebaseDB/Views/Card (Tab Menu)/CardInformationView.swift
@@ -0,0 +1,154 @@
+//
+//  CardInformationView.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/07/29.
+//
+
+import SwiftUI
+
+struct CardInformationView: View {
+    
+    @State var cardNumber:String = "0000 0000 0000 0000"
+    @State var expDate:String = "02/23"
+    @State var cvvNumber:String = "000"
+    
+    let lightGreen = Color("LightGreen")
+    
+    @State private var showExpDateField = false
+    @State private var showCvvField = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack{
+                lightGreen
+                    .frame(width: .infinity, height: 450)
+                
+                VStack {
+                    Text("Card Number")
+                        .bold()
+                    TextField("Card Number", text: $cardNumber)
+                        .padding()
+                        .frame(width: .infinity, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(15)
+                        .padding(1)
+                    
+                    Text("Expiration Date")
+                        .bold()
+                    HStack{
+                        ZStack{
+                            if showExpDateField == false {
+                                SecureField("Password", text: $expDate)
+                                    .padding()
+                                    .frame(width: .infinity, height: 50)
+                                    .background(Color.black.opacity(0.05))
+                                    .cornerRadius(15)
+                                    .padding(1)
+                            } else {
+                                TextField("Password", text: $expDate)
+                                    .padding()
+                                    .frame(width: .infinity, height: 50)
+                                    .background(Color.black.opacity(0.05))
+                                    .cornerRadius(15)
+                                    .padding(1)
+                            }
+                            
+                        }
+                    
+                    }.overlay(alignment: .trailing){
+                        Image(systemName: showExpDateField ? "eye": "eye.slash")
+                            .onTapGesture {
+                                showExpDateField.toggle()
+                            }
+                            .padding()
+                    }
+                    
+                    Text("Expiration Date")
+                        .bold()
+                    HStack{
+                        ZStack{
+                            if showCvvField == false {
+                                SecureField("Password", text: $cvvNumber)
+                                    .padding()
+                                    .frame(width: .infinity, height: 50)
+                                    .background(Color.black.opacity(0.05))
+                                    .cornerRadius(15)
+                                    .padding(1)
+                            } else {
+                                TextField("Password", text: $cvvNumber)
+                                    .padding()
+                                    .frame(width: .infinity, height: 50)
+                                    .background(Color.black.opacity(0.05))
+                                    .cornerRadius(15)
+                                    .padding(1)
+                            }
+                            
+                        }
+                        
+                    }.overlay(alignment: .trailing){
+                        Image(systemName: showCvvField ? "eye": "eye.slash")
+                            .onTapGesture {
+                                showCvvField.toggle()
+                            }
+                            .padding()
+                    }
+                    
+                    NavigationLink(
+                    destination: HelpDeskView(),
+                    label: {
+                        HStack{
+                            Image(systemName: "creditcard.trianglebadge.exclamationmark")
+                                .resizable()
+                                .frame(width: 40, height: 25)
+                            Text("Lost Card")
+                                .frame(alignment: .leading)
+                                .foregroundColor(.black)
+                                .font(.headline)
+                                .cornerRadius(15)
+                            Spacer()
+                        }
+                        .padding(.bottom)
+                        .padding(.top)
+                        
+                    })
+                        
+                        NavigationLink(
+                        destination: HelpDeskView(),
+                        label: {
+                            HStack{
+                                Image(systemName: "rectangle.badge.plus")
+                                    .resizable()
+                                    .frame(width: 40, height: 25)
+                                Text("Request New Card")
+                                    .frame(alignment: .leading)
+                                    .foregroundColor(.black)
+                                    .font(.headline)
+                                    .cornerRadius(15)
+                                Spacer()
+                            }
+                            .padding(.bottom)
+                            .padding(.top)
+                        
+                    })
+                    
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(15)
+                .shadow(color: .black.opacity(0.25), radius: 5, x: 3, y: -5)
+                    .navigationViewStyle(.stack)
+                    .navigationBarBackButtonHidden(true)
+                    .navigationBarHidden(true)
+            }
+            
+        }
+        
+    }
+}
+
+struct CardInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardInformationView()
+    }
+}
