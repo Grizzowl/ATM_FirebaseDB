ee2c843b99ec84920ecad51e1c674b8bf708fbee
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-07-29 11:07:25 +0200

    [ATM2.0Firebase][Feature] Help desk Information added
    If users need assistance with card-related issues this view has the relevant contact information

---
+++ ATM_FirebaseDB/Views/Card (Tab Menu)/HelpDeskView.swift
@@ -0,0 +1,36 @@
+//
+//  HelpDeskView.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/07/28.
+//
+
+import SwiftUI
+
+struct HelpDeskView: View {
+    var body: some View {
+        List {
+            Text("Customer Service landline")
+            HStack{
+                Image(systemName: "phone.and.waveform")
+                    .padding(.trailing, 25)
+                Text("011 555 5656")
+                    .foregroundColor(.blue)
+            }
+            
+            Text("Customer Service Email")
+            HStack{
+                Image(systemName: "envelope")
+                    .padding(.trailing, 25)
+                Text("helpdesk@grizzbankltd.co.za")
+                    .foregroundColor(.blue)
+            }
+        }
+    }
+}
+
+struct HelpDeskView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelpDeskView()
+    }
+}
