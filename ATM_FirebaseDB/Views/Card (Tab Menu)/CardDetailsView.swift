e3814984803edc6592770bf7935240daff8cfeee
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-07-29 11:15:02 +0200

    [ATM2.0Firebase][UI Feature] Card details final screen
    This screen composites the different views into one view that will be shown on the tab menu in the app when clicked

---
+++ ATM_FirebaseDB/Views/Card (Tab Menu)/CardDetailsView.swift
@@ -0,0 +1,38 @@
+//
+//  CardDetailsView.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/07/28.
+//
+
+import SwiftUI
+
+struct CardDetailsView: View {
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                Color(.white)
+                    .edgesIgnoringSafeArea(.all)
+                VStack{
+                    CardTemplateView()
+                        .padding()
+                    VStack(spacing: -610){
+                        UserCardDetailsHeader()
+                        VStack {
+                            CardInformationView()
+                        }
+                    }
+                }
+                .navigationTitle(Text("My Card"))
+            }
+        }
+    }
+}
+
+struct CardDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardDetailsView()
+    }
+}
