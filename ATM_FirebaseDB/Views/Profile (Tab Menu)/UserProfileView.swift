76d76658fb2b60e027b23af6cd01bc877a054afd
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-02 14:30:48 +0200

    [ATM2.0Firebase][Feature] Crated UserProfileView.swift
    This view composes the various elements needed for the users profile view landing page on the tab menu

---
+++ ATM_FirebaseDB/Views/Profile (Tab Menu)/UserProfileView.swift
@@ -0,0 +1,37 @@
+//
+//  UserProfileView.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/08/02.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                Color(.white)
+                    .edgesIgnoringSafeArea(.all)
+                
+                    VStack(spacing: -610){
+                        UserProfileHeader()
+                        VStack {
+                            UserProfileDetails()
+                        }
+                    
+                }
+                
+            }
+            .navigationTitle(Text("My Profile"))
+        }
+    }
+}
+
+struct UserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfileView()
+    }
+}
