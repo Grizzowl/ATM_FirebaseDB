4be26f9914e1a04eed8f0216e9be7bd190481532
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-05 15:36:13 +0200

    [ATM2.0FireB][Feature] Added a func to create a new user to firebase

---
+++ ATM_FirebaseDB/ViewModel/CreateNewUser.swift
@@ -0,0 +1,26 @@
+//
+//  CreateNewUser.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/08/05.
+//
+
+import Foundation
+import FirebaseAuth
+import Firebase
+import FirebaseCore
+
+class CreateNewUser: ObservableObject {
+    
+    func createNewUser(email:String, password:String){
+        
+        Auth.auth().createUser(withEmail: email, password: password) {
+            result, error in
+            if let error = error {
+                print("failed to create user:", error)
+            }
+        }
+    }
+    
+}
+
