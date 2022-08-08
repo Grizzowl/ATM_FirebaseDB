486b36d15b5fd393b2c6cd41b0402deff74fd38e
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-08 10:25:20 +0200

    [ATM2.0FireB][Feature] A Error page view so the user knows something went wrong
    WIP this will be added on at a later stage

---
+++ ATM_FirebaseDB/Views/Registration /CreateNewError.swift
@@ -0,0 +1,20 @@
+//
+//  signUpError.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/08/08.
+//
+
+import SwiftUI
+
+struct CreateNewError: View {
+    var body: some View {
+        Image(systemName: "person.crop.circle.badge.exclamationmark")
+    }
+}
+
+struct signUpError_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateNewError()
+    }
+}
