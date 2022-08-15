28ae16a87b6780327bf642da5fe3aea138941ec0
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-15 11:17:29 +0200

    [ATM2.0FireB][Feature] Added Func's to auto gen user information
    Some fields are required for account creation and they are not filled by the user rather these functions in (Registration Number Auto Generation) handle the generation of the values

---
+++ ATM_FirebaseDB/ViewModel/Registration Number Auto Generation/autoFillFuncModel.swift
@@ -0,0 +1,45 @@
+//
+//  autoFillFuncModel.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/08/15.
+//
+
+import Foundation
+
+class autoFillFuncModel: ObservableObject {
+    
+    // EXP DATE
+   
+    func setCardExpDate() -> String{
+        
+        let currentDate = Date()
+        var cardExpDatemodifier = DateComponents()
+        cardExpDatemodifier.year = 5
+        let cardExpDate = Calendar.current.date(byAdding: cardExpDatemodifier, to: currentDate)
+        let expDateMonth = cardExpDate!.formatted(.dateTime.month(.twoDigits))
+        let expDateYear = cardExpDate!.formatted(.dateTime.year(.twoDigits))
+        
+        return ("\(expDateMonth)/\(expDateYear)")
+    }
+    
+    // CARD NUMBER
+    func cardNumberAutoGen() -> String {
+        
+        let bankCodeCardNumber: Int = 4550
+        let cardNumberSegOne = Int.random(in: 1000...9999)
+        let cardNumberSegTwo = Int.random(in: 1000...9999)
+        let cardNumberSegThree = Int.random(in: 1000...9999)
+        
+        return ("\(bankCodeCardNumber) \(cardNumberSegOne) \(cardNumberSegTwo) \(cardNumberSegThree)")
+    }
+    
+    // CVV NUMBER
+    func cvvAutoGen() -> String {
+        
+        let cvvAutoGen = Int.random(in: 100...999)
+        
+        return ("\(cvvAutoGen)")
+    }
+    
+}
