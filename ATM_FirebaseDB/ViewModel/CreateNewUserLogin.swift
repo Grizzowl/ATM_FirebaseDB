acbd948611f00071a2a690676910f58363ba0bbc
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-24 12:59:22 +0200

    [ATM2.0FireB][WIP] Login method will be updated
    Commented old code for future updating

--- ATM_FirebaseDB/ViewModel/CreateNewUserLogin.swift
+++ ATM_FirebaseDB/ViewModel/CreateNewUserLogin.swift
@@ -5,29 +5,29 @@
 //  Created by Grizzowl on 2022/08/05.
 //
 
-import Foundation
-import FirebaseAuth
-import Firebase
+//import Foundation
+//import FirebaseAuth
+//import Firebase
 
-class CreateNewUserLogin: ObservableObject {
-    
-    let auth = Auth.auth()
-    
-    func createNewUser(email:String, password:String){
-        
-        auth.createUser(withEmail: email, password: password) { result, error in
-            if let error = error {
-                print("Failed to create user:", error)
-               
-                return
-            }
-            
-            print("Successfully created user: \(result?.user.uid ?? "")")
-            
-            
-        }
-    }
-    
-}
+//class CreateNewUserLogin: ObservableObject {
+//
+//    let auth = Auth.auth()
+//
+//    func createNewUser(email:String, password:String){
+//
+//        auth.createUser(withEmail: email, password: password) { result, error in
+//            if let error = error {
+//                print("Failed to create user:", error)
+//
+//                return
+//            }
+//
+//            print("Successfully created user: \(result?.user.uid ?? "")")
+//
+//
+//        }
+//    }
+//
+//}
     
 
