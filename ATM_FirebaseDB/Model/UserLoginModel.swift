87e13b716f19b5aab11eb959d04c85a0781b4477
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-26 11:12:14 +0200

    [ATM2.0FireB][Refactor] User Login Model
    Logic has been added to get user login details and check with the database

---
+++ ATM_FirebaseDB/Model/UserLoginModel.swift
@@ -0,0 +1,47 @@
+//
+//  UserLoginModel.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/08/25.
+//
+
+import Foundation
+import Combine
+import Firebase
+
+protocol LoginService {
+    func login(with credentials: UserLoginCredentials) -> AnyPublisher<Void, Error>
+}
+
+struct UserLoginCredentials {
+    var email: String
+    var password: String
+}
+
+final class LoginServiceImp: LoginService {
+    
+    func login(with credentials: UserLoginCredentials) -> AnyPublisher<Void, Error> {
+        
+        Deferred {
+            
+            Future { promise in
+                
+                Auth
+                    .auth()
+                    .signIn(withEmail: credentials.email,
+                            password: credentials.password) { res, error in
+                        
+                        if let err = error {
+                            promise(.failure(err))
+                        } else {
+                            promise(.success(()))
+                        }
+                        
+                    }
+                
+            }
+        }
+        .receive(on: RunLoop.main)
+        .eraseToAnyPublisher()
+    }
+}
