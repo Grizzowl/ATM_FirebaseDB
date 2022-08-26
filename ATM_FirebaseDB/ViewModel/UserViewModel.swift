09fb61b3649923e872fe67ac702e441524f79df4
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-26 11:05:20 +0200

    [ATM2.0FireB][Removed] Unused VM

--- ATM_FirebaseDB/ViewModel/UserViewModel.swift
+++ ATM_FirebaseDB/ViewModel/UserViewModel.swift
@@ -9,26 +9,3 @@ import Foundation
 import Firebase
 import FirebaseCore
 
-class UserViewModel: ObservableObject {
-    
-    @Published var userAccount = [RegistrationDetails]()
-    
-    //get users info from db
-    //home screen
-    func getUserData() {
-        
-    }
-    
-    //update users info
-    func updateData(updateUser: RegistrationDetails) {
-       
-    }
-    
-    //edit account info
-    func editData(){
-        
-    }
-    
-    
-    
-}
