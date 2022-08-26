bb54672ddadc5518004afa229cb01c006b407f12
Grizzowl <107021956+Grizzowl@users.noreply.github.com>
2022-08-26 11:08:10 +0200

    [ATM2.0FireB][Feature] Login Model
    Users can now log in with email and password and change the app session state base on the login status

---
+++ ATM_FirebaseDB/ViewModel/LoginViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  LoginViewModel.swift
+//  ATM_FirebaseDB
+//
+//  Created by Grizzowl on 2022/08/24.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+protocol LoginViewModel {
+    func login()
+    var loginUser: LoginService { get }
+    var state: LoginState { get }
+    var hasError: Bool { get }
+    var loginDetails: UserLoginCredentials { get }
+    init(service: LoginService)
+}
+
+enum LoginState {
+    case successfullyLoggedIn
+    case failed(error: Error)
+    case na
+}
+
+final class LoginViewModelImp: ObservableObject, LoginViewModel {
+    
+    let loginUser: LoginService
+    @Published var state: LoginState = .na
+    @Published var loginDetails: UserLoginCredentials = UserLoginCredentials(email:"",
+                                                                             password:"")
+    
+    @Published var hasError: Bool = false
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init(service: LoginService) {
+        self.loginUser = service
+        setupErrorSubscription()
+    }
+    
+    func login() {
+        loginUser
+            .login(with: loginDetails)
+            .sink { res in
+                switch res {
+                case .failure(let err):
+                    self.state = .failed(error: err)
+                default: break
+                }
+            } receiveValue: { [weak self] in
+                self?.state = .successfullyLoggedIn
+            }
+            .store(in: &subscriptions)
+    }
+}
+
+private extension LoginViewModelImp {
+    
+    func setupErrorSubscription() {
+        $state
+            .map { state -> Bool in
+                switch state {
+                case .successfullyLoggedIn,
+                        .na:
+                    return false
+                case .failed:
+                    return true
+                }
+            }
+            .assign(to: &$hasError)
+    }
+}
